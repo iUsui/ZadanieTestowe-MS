8cf8658ae50e3d7cd1b0c5cd7565f12135715957
Maciek <maciek.wol16@gmail.com>
2022-04-13 11:28:43 +0200

    8 - special projectile cooldown UI display

--- Assets/__Scripts/Player/PlayerController.cs
+++ Assets/__Scripts/Player/PlayerController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -13,6 +14,9 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private GameObject projectilePrefab = null;
     [SerializeField] private GameObject specialProjectilePrefab = null;
     [SerializeField] private float specialProjectileCooldown = 2.0f;
+    [SerializeField] private TMP_Text textCooldown = null;
+    private bool canShootSpecialProjectile = true;
+    private float shootTime = 0;
     
     private List<SpecialProjectile> specialProjectiles = new List<SpecialProjectile>();
     
@@ -48,19 +52,18 @@ public class PlayerController : MonoBehaviour
             currentPosition += new Vector3(previousInput.x, 0f, 0f) * movementSpeed * Time.deltaTime;
         }
         transform.position = currentPosition;
-        if (Input.GetKeyDown(KeyCode.Y)) {
-            if (specialProjectiles.Count != 0) {
-                foreach (var specialProjectile in specialProjectiles) {
-                    specialProjectile.Explode();
-                }
-                return;
+        if (Input.GetKeyDown(KeyCode.Y) && canShootSpecialProjectile) {
+            ShootingSpecialProjectile();
+        }
+        else if (!canShootSpecialProjectile) {
+            float displayNumber = specialProjectileCooldown - (Time.realtimeSinceStartup - shootTime);
+            textCooldown.text = displayNumber.ToString("F2");
+            if (displayNumber <= 0) { 
+                textCooldown.text = "READY!";
+                canShootSpecialProjectile = true;
             }
-            GameObject newSpecialProjectile = specialProjectilePrefab;
-            newSpecialProjectile.GetComponent<SpecialProjectile>().SetAttackDamage(attackDamage);
-            Instantiate(newSpecialProjectile, shootPoint.position, shootPoint.rotation);
         }
     }
-
     private void SetPreviousInput(InputAction.CallbackContext ctx) {
         previousInput = ctx.ReadValue<Vector2>();
     }
@@ -69,6 +72,28 @@ public class PlayerController : MonoBehaviour
         Instantiate(projectilePrefab, shootPoint.position, shootPoint.rotation);
     }
 
+// special projectile
+    private void ShootingSpecialProjectile() {
+        if (specialProjectiles.Count != 0) {
+                foreach (var specialProjectile in specialProjectiles) {
+                    specialProjectile.Explode();
+                }
+                // StartCoroutine(SetCanShootSpecialProjectile());
+                shootTime = Time.realtimeSinceStartup;
+                canShootSpecialProjectile = false;
+                return;
+            }
+            GameObject newSpecialProjectile = specialProjectilePrefab;
+            newSpecialProjectile.GetComponent<SpecialProjectile>().SetAttackDamage(attackDamage);
+            Instantiate(newSpecialProjectile, shootPoint.position, shootPoint.rotation);
+    }
+    private IEnumerator SetCanShootSpecialProjectile() {
+        canShootSpecialProjectile = false;
+        yield return new WaitForSeconds(specialProjectileCooldown);
+        canShootSpecialProjectile = true;
+    }
+
+// event handlers
     private void HandleOnSpawnedSpecialProjectile(SpecialProjectile projectile)
     {
         specialProjectiles.Add(projectile);
